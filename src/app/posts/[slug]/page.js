4de2b7978f8081b62b32f7a75a6cd41e21fe70cf
brenno luan <brennoluan8530@gmail.com>
2026-06-18 21:29:59 -0300

    implement data fetching for individual posts using @tanstack/react-query

--- src/app/posts/[slug]/page.js
+++ src/app/posts/[slug]/page.js
@@ -1,13 +1,23 @@
-"use client";
+'use client';
 
-import { CardPost } from "@/components/CardPost";
-import { CommentList } from "@/components/CommentList";
-import styles from "./page.module.css";
+import { CardPost } from '@/components/CardPost';
+import { CommentList } from '@/components/CommentList';
+import styles from './page.module.css';
+import { useQuery } from '@tanstack/react-query';
+
+const fetchPostBySlug = async ({ slug }) => {
+  const results = await fetch(`http://localhost:3000/api/posts/${slug}`);
+  const data = await results.json();
+  return data;
+};
 
 const PagePost = ({ params }) => {
   const { slug } = params;
 
-  const post = null;
+  const { data: post } = useQuery({
+    queryKey: ['post', slug],
+    queryFn: () => fetchPostBySlug({ slug }),
+  });
 
   const postRating = null;
 
