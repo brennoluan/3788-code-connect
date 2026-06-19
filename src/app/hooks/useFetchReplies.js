21a70a3550c2aed6107137c096dd691759d461e1
brenno luan <brennoluan8530@gmail.com>
2026-06-18 22:04:47 -0300

    export fetchReplies function and update queryKey in useFetchReplies hook to include slug for improved data fetching

--- src/app/hooks/useFetchReplies.js
+++ src/app/hooks/useFetchReplies.js
@@ -1,6 +1,6 @@
 import { useQuery } from '@tanstack/react-query';
 
-const fetchReplies = async ({ commentId, slug }) => {
+export const fetchReplies = async ({ commentId, slug }) => {
   const response = await fetch(
     `/api/comment/${commentId}/replies?slug=${slug}`,
   );
@@ -14,7 +14,7 @@ const fetchReplies = async ({ commentId, slug }) => {
 
 export const useFetchReplies = ({ commentId, slug }) => {
   return useQuery({
-    queryKey: ['replies', commentId],
+    queryKey: ['replies', commentId, slug],
     queryFn: async () => fetchReplies({ commentId, slug }),
     enabled: !!commentId && !!slug,
     retry: 5,
