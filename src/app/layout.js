93f1f7417109a2a1356a3e35953ae5db1cdf22c5
brenno luan <brennoluan8530@gmail.com>
2026-06-18 21:21:17 -0300

    refactor layout to include ReactQueryProvider and improve structure

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,33 +1,36 @@
-import { Prompt } from "next/font/google";
-import { Aside } from "@/components/Aside";
-import "./globals.css";
-import { SearchForm } from "@/components/SearchForm";
+import { Prompt } from 'next/font/google';
+import { Aside } from '@/components/Aside';
+import './globals.css';
+import { SearchForm } from '@/components/SearchForm';
+import { ReactQueryProvider } from './ReactQueryProvider';
 
 export const metadata = {
-  title: "Code Connect",
-  description: "Uma rede social para devs!",
+  title: 'Code Connect',
+  description: 'Uma rede social para devs!',
 };
 
 const prompt = Prompt({
-  weight: ["400", "500", "600"],
-  subsets: ["latin"],
-  display: "swap",
+  weight: ['400', '500', '600'],
+  subsets: ['latin'],
+  display: 'swap',
 });
 
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-br" className={prompt.className}>
-      <body>
-        <div className="app-container">
-          <div>
-            <Aside />
+      <ReactQueryProvider>
+        <body>
+          <div className="app-container">
+            <div>
+              <Aside />
+            </div>
+            <div className="main-content">
+              <SearchForm />
+              {children}
+            </div>
           </div>
-          <div className="main-content">
-            <SearchForm />
-            {children}
-          </div>
-        </div>
-      </body>
+        </body>
+      </ReactQueryProvider>
     </html>
   );
 }
